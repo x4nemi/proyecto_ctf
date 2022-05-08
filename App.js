8ced816985a10fde023d0b660be7209017bd2bfc
Ximena <ximena.garcia7482@alumnos.udg.mx>
2022-05-07 21:50:56 -0500

    Interfaz hecha

--- App.js
+++ App.js
@@ -1,20 +1,142 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+  TextInput,
+  Button,
+} from "react-native";
+import { BlurView } from "expo-blur";
 
-export default function App() {
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./firebase-config";
+
+const uri = "https://ak.picdn.net/shutterstock/videos/1060308725/thumb/1.jpg";
+const profilePicture = "https://randomuser.me/api/portraits/men/34.jpg";
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
     </View>
   );
+}
+function LoginScreen() {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri }} style={[styles.image, StyleSheet.absoluteFill]} />
+      <ScrollView
+        contentContainerStyle={{
+          flex: 1,
+          width: "100%",
+          height: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <BlurView intensity={90}>
+          <View style={styles.login}>
+            <Image source={{ profilePicture }} style={styles.profilePicture} />
+            <View>
+              <Text style={{ fontSize: 17, fontWeight: "400", color: "white" }}>
+                E-mail
+              </Text>
+              <TextInput
+                style={styles.input}
+                placeholder="motomami@hotmail.com"
+              />
+            </View>
+            <View>
+              <Text style={{ fontSize: 17, fontWeight: "400", color: "white" }}>
+                Contraseña
+              </Text>
+              <TextInput
+                style={styles.input}
+                placeholder="contraseña"
+                secureTextEntry={true}
+              />
+            </View>
+            <TouchableOpacity
+              style={[styles.button, { backgroundColor: "#00CFEB90" }]}
+            >
+              <Text style={{ fontSize: 17, fontWeight: "400", color: "white" }}>
+                Login
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.button, { backgroundColor: "#6792F090" }]}
+            >
+              <Text style={{ fontSize: 17, fontWeight: "400", color: "white" }}>
+                Create Account
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </BlurView>
+      </ScrollView>
+    </View>
+  );
+}
+
+export default function App() {
+  return <LoginScreen />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+  },
+  login: {
+    width: 350,
+    height: 500,
+    borderColor: "#fff",
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    alignItems: "center",
+  },
+  profilePicture: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderColor: "#fff",
+    borderWidth: 1,
+    marginVertical: 30,
+  },
+  input: {
+    width: 250,
+    height: 40,
+    borderColor: "#fff",
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ffffff90",
+    marginBottom: 20,
+  },
+  button: {
+    width: 250,
+    height: 40,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
+    borderColor: "#fff",
+    borderWidth: 1,
   },
 });
---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDOdbHgFdLA5AGOkWRDMz5zFWlKlY-Xbb4",
+  authDomain: "proyectoctf-5353a.firebaseapp.com",
+  projectId: "proyectoctf-5353a",
+  storageBucket: "proyectoctf-5353a.appspot.com",
+  messagingSenderId: "871049866225",
+  appId: "1:871049866225:web:c95993ceb3a9039e183250",
+  measurementId: "G-KJ1W25PDEM",
+};
